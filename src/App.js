37728c296014cbe0857564af632d12bfca4da0a5
minzlzl <min42595@gmail.com>
2023-09-23 15:48:00 +0900

    a

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>git에 레파지토리 만들기</p>
+      <br/>
+      <p>git init</p>
+      <p>git add README.md</p>
+      <p>git commit -m "first commit"</p>
+      <p>git branch -M main</p>
+      <p>git remote add origin https://github.com/minzlzl/레파지토리명.git</p>
+      <p>git push -u origin main</p>
+      <br/>
+      <p>settings가서 pages클릭 후 Branch main 선택하고 저장</p>
+      <br/>
+      <p>터미널창에 npm i gh-pages</p>
+      <p>package.json파일 scripts부분에 "predeploy": "npm run build", "deploy": "gh-pages -d build"</p>
+      <p>package.json파일 최상단에 "homepage": "https://minzlzl.github.io/레파지토리명"</p>
+      <p>npm run predeploy</p>
+      <p>npm run deploy</p>
     </div>
   );
 }
